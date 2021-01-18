c68d18e173d9748ba1be9b4f550911b67ed2ea38
Suman Nakkala <sumankrishna1988@gmail.com>
2021-01-18 08:47:17 +0530

    jan 18th

--- MyFirstWebapp/src/main/java/com/suman/myapp/HelloServlet.java
+++ MyFirstWebapp/src/main/java/com/suman/myapp/HelloServlet.java
@@ -1,23 +1,46 @@
 package com.suman.myapp;
 
-import java.io.PrintWriter;
-
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
 
-@WebServlet(urlPatterns = "/hello")
+//@WebServlet(urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
-
+	
+	
+	String webSiteName;
+	
+	
+	public void init(ServletConfig servletConfig) throws ServletException {
+		
+		System.out.println("I AM IN INIT METHOD..!!!");
+		this.webSiteName=servletConfig.getInitParameter("websitename");
+		
+		
+		
+	}
+	
+	
 	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws javax.servlet.ServletException, java.io.IOException {
 		
+		
+		System.out.println("THE WEBSITE is " + webSiteName);
+		
+		
+		String contextParam = request.getSession().getServletContext().getInitParameter("commonName");
+		
+		System.out.println("THE CONTEXT PARAMM is " + contextParam);
+		
 		String param = request.getParameter("name");
+		
+		//System.out.println("Checking the Init Parameter--" + myParam);
 		
 		request.setAttribute("name", param);
 		
